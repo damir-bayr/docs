0748778657cce01fffe2144cba5e03f8a347938a
Damir Bajramovic <bayr.damir@gmail.com>
2025-08-07 10:15:33 +0200

    Add DMP dropdown.

---
+++ scripts/do-not-lose.sh
@@ -0,0 +1,30 @@
+# ,
+#     "global": {
+#       "anchors": [
+#         {
+#           "anchor": "deBridge Home",
+#           "href": "https://acme-68677d63.mintlify.app/",
+#           "icon": "house"
+#         },
+#         {
+#           "anchor": "deBridge Messaging Protocol",
+#           "href": "https://acme-fc8786fa.mintlify.app/dmp/protocol-overview",
+#           "icon": "envelope"
+#         },
+#         {
+#           "anchor": "deBridge Liquidity Network",
+#           "href": "https://acme-1c46979f.mintlify.app/overview/introduction",
+#           "icon": "envelope-open-dollar"
+#         },
+#         {
+#           "anchor": "Gasless DLN",
+#           "href": "https://acme-ad8c06cd.mintlify.app/overview/executive-summary",
+#           "icon": "gas-pump-slash"
+#         },
+#         {
+#           "anchor": "Community",
+#           "href": "https://mintlify.com/community",
+#           "icon": "discord"
+#         }
+#       ]
+#     }
